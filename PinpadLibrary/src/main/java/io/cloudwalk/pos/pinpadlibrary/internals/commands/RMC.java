a491db12d2abc07162f6c0b7cdeae30701a1ac9e
Maurício Spinardi <mauricio.spinardi@cloudwalk.io>
2022-06-30 17:35:34 -0300

    reenable RMC

--- PinpadLibrary/src/main/java/io/cloudwalk/pos/pinpadlibrary/internals/commands/RMC.java
+++ PinpadLibrary/src/main/java/io/cloudwalk/pos/pinpadlibrary/internals/commands/RMC.java
@@ -1,8 +1,16 @@
 package io.cloudwalk.pos.pinpadlibrary.internals.commands;
 
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static java.util.Locale.US;
+
 import org.jetbrains.annotations.NotNull;
+import org.json.JSONObject;
+
+import java.io.ByteArrayOutputStream;
 
 import io.cloudwalk.loglibrary.Log;
+import io.cloudwalk.pos.pinpadlibrary.ABECS;
+import io.cloudwalk.utilitieslibrary.utilities.ByteUtility;
 
 public class RMC {
     private static final String
@@ -12,27 +20,67 @@ public class RMC {
             throws Exception {
         Log.d(TAG, "parseRequestDataPacket");
 
-        throw new RuntimeException("TODO: parseRequestDataPacket");
+        JSONObject request = new JSONObject();
+
+        request.put(ABECS.CMD_ID,  new String(array, 0,  3));
+        request.put(ABECS.RMC_MSG, new String(array, 6, 32));
+
+        return request.toString();
     }
 
     public static String parseResponseDataPacket(byte[] array, int length)
             throws Exception {
         Log.d(TAG, "parseResponseDataPacket");
 
-        throw new RuntimeException("TODO: parseResponseDataPacket");
+        return CMD.parseResponseDataPacket(array, length);
     }
 
     public static byte[] buildRequestDataPacket(@NotNull String string)
             throws Exception {
         Log.d(TAG, "buildRequestDataPacket");
 
-        throw new RuntimeException("TODO: buildRequestDataPacket");
+        ByteArrayOutputStream[] stream = {
+                new ByteArrayOutputStream(),
+                new ByteArrayOutputStream()
+        };
+
+        byte[] CMD_ID  = null;
+        byte[] RMC_MSG = null;
+
+        try {
+            JSONObject request = new JSONObject(string);
+
+            CMD_ID  = request.getString(ABECS.CMD_ID) .getBytes(UTF_8);
+            RMC_MSG = request.getString(ABECS.RMC_MSG).getBytes(UTF_8);
+
+            stream[0].write(CMD_ID);
+            stream[1].write(RMC_MSG);
+
+            byte[] CMD_DATA = null;
+
+            try {
+                CMD_DATA = stream[1].toByteArray();
+
+                stream[0].write(String.format(US, "%03d", CMD_DATA.length).getBytes(UTF_8));
+                stream[0].write(CMD_DATA);
+            } finally {
+                ByteUtility.clear(CMD_DATA);
+            }
+
+            byte[] array = stream[0].toByteArray();
+
+            return array;
+        } finally {
+            ByteUtility.clear(stream);
+
+            ByteUtility.clear(CMD_ID, RMC_MSG);
+        }
     }
 
     public static byte[] buildResponseDataPacket(@NotNull String string)
             throws Exception {
         Log.d(TAG, "buildResponseDataPacket");
 
-        throw new RuntimeException("TODO: buildResponseDataPacket");
+        return CMD.buildResponseDataPacket(string);
     }
 }
